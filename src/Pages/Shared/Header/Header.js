1af863c96edd2a488689278e898bb8292760e550
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-08-31 00:06:15 +0600

    header design done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo2.png';
 
 const Header = () => {
     return (
         <div>
+            <div className='header'>
+                <div>
+                    <img style={{ height: '40px' }} src={logo} alt="" />
+                </div>
+                <div>
+                    <ul className="nav">
+                        <Link className='links' to="/cart">Cart</Link>
+                        <Link className='links' to="/login">Login</Link>
+                        <Link className='links btn' to="/register">Sign Up</Link>
+                    </ul>
+                </div>
+            </div>
 
         </div>
     );
