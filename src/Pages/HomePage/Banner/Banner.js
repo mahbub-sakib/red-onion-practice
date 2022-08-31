a928d1af2485860d2ec2722c73416c991799b00f
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-09-01 00:53:57 +0600

    banner search designed

--- src/Pages/HomePage/Banner/Banner.js
+++ src/Pages/HomePage/Banner/Banner.js
@@ -7,7 +7,13 @@ const Banner = () => {
         <div>
             <div className='banner'>
                 <img className='banner-img' src={banner} alt="" />
+                <div className='banner-search'>
+                    <input type="text" placeholder='Search food items' maxlength="50" />
+                    <button>Search</button>
+                </div>
+                <h1>Best food Waiting for your belly</h1>
             </div>
+
         </div>
     );
 };
